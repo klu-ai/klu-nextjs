1f5db143671b5a344448fdfbb36827cda25b6d63
Abiel Zulio M <abielzm@gmail.com>
2023-09-17 22:09:09 +0700

    missing

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <KluProvider>{children}</KluProvider>
+      </body>
     </html>
   )
 }
