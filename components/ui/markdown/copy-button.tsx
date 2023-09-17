72b6cbb3ef382e20d2304f35308717ace7df347a
Abiel Zulio M <abielzm@gmail.com>
2023-09-17 22:00:31 +0700

    use native clipboard

--- components/ui/markdown/copy-button.tsx
+++ components/ui/markdown/copy-button.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react"
-import CopyToClipboard from "react-copy-to-clipboard"
 import { Clipboard, ClipboardCheck } from "lucide-react"
 import { cn } from "@/utils/cn"
 import { Button } from "../button"
@@ -19,6 +18,7 @@ const CopyButton = ({ text }: { text: string }) => {
   }, [effect])
 
   const handleCopy = () => {
+    navigator.clipboard.writeText(text)
     setEffect(true)
     setCopied(true)
     setTimeout(() => {
@@ -28,28 +28,26 @@ const CopyButton = ({ text }: { text: string }) => {
 
   return (
     <div className="flex items-center">
-      <CopyToClipboard text={text} onCopy={handleCopy}>
-        <Button
-          className={cn(
-            effect
-              ? "text-green-500 border-green-400"
-              : "text-black/50 hover:text-black/100",
-            "inline-flex items-center focus:outline-none w-fit h-fit p-1 text-[10px] font-sans"
-          )}
-          variant="secondary"
-          onClick={handleCopy}
-          onAnimationEnd={() => {
-            setEffect(false)
-          }}
-          tooltip="Copy"
-          icon={{
-            icon: copied ? ClipboardCheck : Clipboard,
-            size: 14,
-          }}
-        >
-          {copied && "Copied!"}
-        </Button>
-      </CopyToClipboard>
+      <Button
+        className={cn(
+          effect
+            ? "text-green-500 border-green-400"
+            : "text-black/50 hover:text-black/100",
+          "inline-flex items-center focus:outline-none w-fit h-fit p-1 text-[10px] font-sans"
+        )}
+        variant="secondary"
+        onClick={handleCopy}
+        onAnimationEnd={() => {
+          setEffect(false)
+        }}
+        tooltip="Copy"
+        icon={{
+          icon: copied ? ClipboardCheck : Clipboard,
+          size: 14,
+        }}
+      >
+        {copied && "Copied!"}
+      </Button>
     </div>
   )
 }
