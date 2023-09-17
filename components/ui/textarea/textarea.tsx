b4dafef8018d0b18ac1cb8d732d6e6ed07707a40
Abiel Zulio M <abielzm@gmail.com>
2023-09-17 15:31:22 +0700

    feat: `textarea`

---
+++ components/ui/textarea/textarea.tsx
@@ -0,0 +1,25 @@
+import { cn } from "@/utils/cn"
+import * as React from "react"
+
+/* export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+} */
+
+const Textarea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.TextareaHTMLAttributes<HTMLTextAreaElement>
+>(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      className={cn(
+        "flex min-h-[60px] w-full rounded-md border border-black/10 bg-transparent  px-3 py-2 text-sm shadow-sm placeholder:text-black/30 focus-visible:bg-black/[0.025] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-1 focus-visible:ring-black/50 disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
+})
+Textarea.displayName = "Textarea"
+
+export { Textarea }
