6449e59b79f5d012fd883e056411a959d7b26a61
Abiel Zulio M <abielzm@gmail.com>
2023-09-17 14:00:13 +0700

    feat: `tabs`

---
+++ components/ui/tabs/tabs.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import * as React from "react"
+import * as TabsPrimitive from "@radix-ui/react-tabs"
+
+import { cn } from "@/utils/cn"
+
+const Root = TabsPrimitive.Root
+
+const List = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.List
+    ref={ref}
+    className={cn(
+      "inline-flex h-9 items-center rounded-lg bg-black/[0.05] p-1 text-black/50 gap-[5px] border-[1px] border-black/5 w-full",
+      className
+    )}
+    {...props}
+  />
+))
+List.displayName = TabsPrimitive.List.displayName
+
+const Trigger = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Trigger
+    ref={ref}
+    className={cn(
+      "inline-flex items-center w-full justify-center whitespace-nowrap rounded-md px-3 py-1 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-black data-[state=active]:outline-[1px] data-[state=active]:outline-black/10 data-[state=active]:shadow hover:bg-black/5 hover:text-black/60",
+      className
+    )}
+    {...props}
+  />
+))
+Trigger.displayName = TabsPrimitive.Trigger.displayName
+
+const Content = React.forwardRef<
+  React.ElementRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ className, ...props }, ref) => (
+  <TabsPrimitive.Content
+    ref={ref}
+    className={cn(
+      "mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      className
+    )}
+    {...props}
+  />
+))
+Content.displayName = TabsPrimitive.Content.displayName
+
+export { Root, List, Trigger, Content }
