53b214df37fac8ccc7eb2b96f24b4425c900fa5b
Abiel Zulio M <abielzm@gmail.com>
2023-09-24 11:32:15 +0700

    color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "off-white": "#f9fafb",
       },
     },
   },
