e9ca14a32a05443ef02adf03329fd6075e704e76
Abiel Zulio M <abielzm@gmail.com>
2023-09-24 11:30:06 +0700

    refactor: `code`

--- components/ui/markdown/code-block.tsx
+++ components/ui/markdown/code-block.tsx
@@ -3,6 +3,26 @@ import { PrismAsyncLight as SyntaxHighlighter } from "react-syntax-highlighter"
 import { a11yLight } from "react-syntax-highlighter/dist/esm/styles/hljs"
 import { cn } from "@/utils"
 import { CopyButton } from "./copy-button"
+import React from "react"
+
+const Code = React.forwardRef<HTMLElement, React.HTMLAttributes<HTMLElement>>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <code
+        className={cn(
+          className,
+          "whitespace-pre-wrap rounded-md bg-zinc-100 px-1.5 py-[3px] pb text-zinc-700 border-black/10 border-[1px]"
+        )}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </code>
+    )
+  }
+)
+
+Code.displayName = "Code"
 
 const CodeBlock: Components["code"] = ({ inline, className, children }) => {
   const match = /language-(\w+)/.exec(className || "")
@@ -23,15 +43,8 @@ const CodeBlock: Components["code"] = ({ inline, className, children }) => {
       </SyntaxHighlighter>
     </div>
   ) : (
-    <code
-      className={cn(
-        className,
-        "whitespace-pre-wrap rounded-md bg-zinc-100 px-1.5 py-[3px] pb text-[12px] text-zinc-700 border-black/10 border-[1px]"
-      )}
-    >
-      {children}
-    </code>
+    <Code className={cn("text-[12px]", className)}>{children}</Code>
   )
 }
 
-export { CodeBlock }
+export { CodeBlock, Code }
