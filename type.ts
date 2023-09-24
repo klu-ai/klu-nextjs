76cd6ddcd7aecf2c0a599b03ff3c84e59d4945f2
Abiel Zulio M <abielzm@gmail.com>
2023-09-24 11:32:45 +0700

    type

---
+++ type.ts
@@ -0,0 +1,30 @@
+export type Stored<T> = T & {
+  storedAt: string
+  revalidatedAt: string
+}
+
+export type Action = {
+  guid: string
+  name: string
+  description: string
+  prompt: string
+  createdById: string
+  model: string
+  app: string
+  createdAt: string
+  updatedAt: string
+  variables: string[]
+}
+
+export type StoredAction = Stored<Action>
+
+export type ActionResponse = {
+  msg: string
+  data_guid: string
+  feedbackUrl: string
+  streaming: boolean
+  actionGuid: string
+  input: string
+}
+
+export type StoredActionResponse = Omit<Stored<ActionResponse>, "revalidatedAt">
