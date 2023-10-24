06398311c61b5356b15705b435ea7644478ddbe5
Abiel Zulio M <abielzm@gmail.com>
2023-10-24 07:48:15 +0700

    refactor: `StreamingTextResponse`

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -1,26 +1,7 @@
 import klu from "@/libs/klu"
 import { readableFromAsyncIterable } from "@/utils/klu"
-import { NextRequest, NextResponse } from "next/server"
-
-export class StreamingTextResponse extends NextResponse {
-  constructor(res: ReadableStream, init?: ResponseInit, data?: any) {
-    let processedStream = res
-
-    if (data) {
-      processedStream = res.pipeThrough(data.stream)
-    }
-
-    super(processedStream as any, {
-      ...init,
-      status: 200,
-      headers: {
-        "Content-Type": "text/plain; charset=utf-8",
-        "X-Experimental-Stream-Data": data ? "true" : "false",
-        ...init?.headers,
-      },
-    })
-  }
-}
+import { StreamingTextResponse } from "@/utils/stream"
+import { NextRequest } from "next/server"
 
 export async function POST(req: NextRequest) {
   const { input, id } = await req.json()
